bcb9c1c4eb771297a402fb85dc8564bc317a5261
abdulelah hakim <DollahHakeem@users.noreply.github.com>
2015-10-14 15:24:55 +0100

    Update server.py

--- Lab5/my_application/server.py
+++ Lab5/my_application/server.py
@@ -3,7 +3,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return 'Hello World!'
+@app.route("/index")
+def index():
+    return 'index'
+@app.route("/hello")
+def helloworld():
+    return 'Hello World!'
+@app.route("/user/paul")
+def userpaul():
+    return 'user paul'
+@app.route("/post/80")
+def post80():
+    return 'post 80'
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0',	port=5000,debug=True)	
+	app.run(host='0.0.0.0',	port=8080,debug=True)	
